b0a552ea62ea684005eae491d6ca2fdbcc34a5a2
VictoriaShyika <victoriashyika@gmail.com>
2024-04-26 23:22:05 +0300

    fixes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,9 @@
 import { TCssPseudoClassesReference, TCssSelectorsReference, TFilesReference, THtmlAttributesReference, THtmlElementsReference, THtmlGlobalAttributesReference } from "./types/types";
-declare const cssPseudoClassesReferences: TCssPseudoClassesReference[];
-declare const cssSelectorsReferences: TCssSelectorsReference[];
-declare const filesReferences: TFilesReference[];
-declare const htmlAttributesReferences: THtmlAttributesReference[];
-declare const htmlElementsReferences: THtmlElementsReference[];
-declare const htmlGlobalAttributesReferences: THtmlGlobalAttributesReference[];
+import cssPseudoClassesReferences from "./references/CSS Pseudo Classes.csv";
+import cssSelectorsReferences from "./references/CSS Selectors.csv";
+import filesReferences from "./references/Files.csv";
+import htmlAttributesReferences from "./references/HTML Attributes.csv";
+import htmlElementsReferences from "./references/HTML Elements.csv";
+import htmlGlobalAttributesReferences from "./references/HTML Global Attributes.csv";
 export { cssPseudoClassesReferences, cssSelectorsReferences, filesReferences, htmlAttributesReferences, htmlElementsReferences, htmlGlobalAttributesReferences, };
 export type { TCssPseudoClassesReference, TCssSelectorsReference, TFilesReference, THtmlAttributesReference, THtmlElementsReference, THtmlGlobalAttributesReference, };
--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,12 @@ import {
   THtmlGlobalAttributesReference,
 } from "./types/types";
 
-import cssPseudoClasses from "./references/CSS Pseudo Classes.csv";
-import cssSelectors from "./references/CSS Selectors.csv";
-import files from "./references/Files.csv";
-import htmlAttributes from "./references/HTML Attributes.csv";
-import htmlElements from "./references/HTML Elements.csv";
-import htmlGlobalAttributes from "./references/HTML Global Attributes.csv";
-
-const cssPseudoClassesReferences: TCssPseudoClassesReference[] = [
-  ...cssPseudoClasses,
-];
-const cssSelectorsReferences: TCssSelectorsReference[] = [...cssSelectors];
-const filesReferences: TFilesReference[] = [...files];
-const htmlAttributesReferences: THtmlAttributesReference[] = [
-  ...htmlAttributes,
-];
-const htmlElementsReferences: THtmlElementsReference[] = [...htmlElements];
-const htmlGlobalAttributesReferences: THtmlGlobalAttributesReference[] = [
-  ...htmlGlobalAttributes,
-];
+import cssPseudoClassesReferences from "./references/CSS Pseudo Classes.csv";
+import cssSelectorsReferences from "./references/CSS Selectors.csv";
+import filesReferences from "./references/Files.csv";
+import htmlAttributesReferences from "./references/HTML Attributes.csv";
+import htmlElementsReferences from "./references/HTML Elements.csv";
+import htmlGlobalAttributesReferences from "./references/HTML Global Attributes.csv";
 
 export {
   cssPseudoClassesReferences,
